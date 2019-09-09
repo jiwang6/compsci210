d1cad56a777f9ca8c2672a3409346a2e0b77f5fa
Jim Wang <jim.z.wang8@gmail.com>
2019-09-09 10:59:25 -0600

    lab 11 sucks

---
+++ Labs/Lab11/lab11.h
@@ -0,0 +1,26 @@
+/** lab11.c
+* ===========================================================
+* Name: C3C Jim Wang, 09 September 2019
+* Section: T3A
+* Project: Lab 11
+* Purpose: for loop practice
+* ===========================================================
+*/
+
+/**   ----------------------------------------------------------
+* <high level description of purpose of the function
+* including what the parameters are used for>
+* @param <explanation of each of the function parameters>
+* @return <explanation of what the function returns>
+*/
+
+#ifndef LAB11_LAB11_H
+#define LAB11_LAB11_H
+
+#include <stdio.h>
+
+int numFactorial();
+int isPrime();
+void floydsTriangle();
+
+#endif //LAB11_LAB11_H
