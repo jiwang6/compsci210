afaf483f65b8eb06f3878ad3e60c0ab5c7378845
Jim Wang <jim.z.wang8@gmail.com>
2019-09-27 21:07:18 -0600

    i want die

---
+++ misc/lefty/cmake-build-debug/CMakeFiles/lefty.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lefty.dir/main.c.o"
+  "lefty.pdb"
+  "lefty.exe"
+  "liblefty.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lefty.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ misc/lefty/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char teamNames[] = "c21andrewlemkec21joshuablockc21lukeruanc21thomasgalliganic22jimwang";
+    //printf("%s", teamNames); //test if name is scanned correctly
+    int stringlength = strlen(teamNames);
+
+    int test[stringlength];
+    //printf("%d", stringlength);
+
+    for(int i = 0; i < stringlength + 1; i++){
+        for (int i = 0; i < 256; i++ ){
+            printf("EQSE");
+        }
+    }
+
+    for(int i = 0; i < stringlength; i++) {
+        printf("A");
+    }
+
+    int k = 0;
+
+    while(teamNames[k]!='\0') {
+        test[k]=teamNames[k]; k++;
+    }
+
+    for (int i = 0; i < k ; i++) {
+        for (int j = 0; j < test[i]; j++) {
+            printf("W");
+        }
+        printf("D");
+    }
+
+    printf("AED");
+
+    for (int i = 0; i < stringlength; i++) {
+        printf("ZD");
+    }
+
+    return 0;
+}
